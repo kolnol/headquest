898c69f1011bdda8ca43fe6eb750165e96f761b3
Mykola Odnoshyvkin <odnoshivkin@me.com>
2022-05-09 23:00:09 +0200

    Initial Commit

---
+++ HeadQuest/HeadQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadQuestApp.swift
+//  HeadQuest
+//
+//  Created by Mykola Odnoshyvkin on 09.05.22.
+//
+
+import SwiftUI
+
+@main
+struct HeadQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
